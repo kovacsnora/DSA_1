f5afad97124513192c869d209c8e12ab950f635e
kovacsnora <kovacs.nora@student.ms.sapientia.ro>
2025-03-12 18:34:52 +0200

    lab03 done

---
+++ lab-03/headers/student.h
@@ -0,0 +1,48 @@
+//
+// Created by norik on 3/6/2025.
+//
+
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Date {
+    int year;
+    int month;
+    int day;
+}Date_t;
+
+typedef enum Gender {
+    FEMALE, MALE
+}Gender_t;
+
+typedef struct Student {
+    char name[50];
+    char neptunCode[8];
+    char birthPlace[25];
+    Date_t birthDate;
+    Gender_t gender;
+    float examResult;
+}Student_t;
+
+void printStudent(Student_t student);
+char *getGenderText(enum Gender gender);
+void allocateMemoryForStudents(Student_t **dpStudents, int numberOfStudents);
+void readStudentDetails(Student_t *pStudent);
+void readAllStudentsDetails(Student_t **dpStudents, int*numberOfStudents, const char *input);
+void allocateMemoryForStudents(Student_t **dpStudents, int numberOfStudents);
+void printAllStudents(Student_t *pStudents, int numberOfStudents, const char *destination);
+void calculatePercentageBoysGirls (Student_t **dpStudents, int numberOfStudents);
+void deallocate(Student_t **pStudent);
+int getNumberOfStudentsByGender(Student_t *pStudents, int numberOfStudents, enum Gender gender);
+void calculatePercentageBoysGirls (Student_t **dpStudents, int numberOfStudents);
+void printStudentsFromSpecificCity(Student_t *pStudents, int numberOfStudents, const char *city);
+void *findStudentByNeptunCode(Student_t *pStudents, int numberOfStudents, const char *neptunCode);
+void sortStudentsByName(Student_t *pStudents, int numberOfStudents);
+void sortStudentsByAgeAndExam(Student_t *pStudents, int numberOfStudents);
+
+
+
+#endif //STUDENT_H
--- lab-03/main.c
+++ lab-03/main.c
@@ -1,6 +1,76 @@
 #include <stdio.h>
+#include "student.h"
 
 int main() {
-    printf("Hello, World!\n");
+    Student_t student1 = {
+        "kovacs nora",
+        "TH3O3I",
+        "marosvasarhely",
+        {2005, 5, 2},
+        FEMALE,
+        8.50
+    };
+
+    printStudent(student1);
+    printf("\n");
+
+    Student_t student2;
+    if (!freopen("student.txt", "r", stdin)) {
+        printf("error");
+        exit(-1);
+    }
+
+    readStudentDetails(&student2);
+    freopen("CON", "r", stdin);
+    printStudent(student2);
+    printf("\n");
+
+    Student_t *pStudents;
+    int numberOfStudents;
+    readAllStudentsDetails(&pStudents, &numberOfStudents, "students.txt" );
+    printf("All students:\n");
+    printAllStudents(pStudents, numberOfStudents, "CON");
+    printAllStudents(pStudents, numberOfStudents, "all_students.txt");
+
+    calculatePercentageBoysGirls(&pStudents, numberOfStudents);
+    printf("\n");
+    printf("\n");
+    printf("Students born in budapest:\n");
+    printStudentsFromSpecificCity(pStudents, numberOfStudents, "Budapest");
+    printf("\n");
+
+    Student_t *result = findStudentByNeptunCode(pStudents, numberOfStudents, "QSTRG9");
+    if (result == NULL ) {
+        printf("this student doesn't exist");
+    }
+    else {
+        printf("QSTRG9 student: ");
+        printf("\n");
+        printStudent(*result);
+    }
+    printf("\n");
+
+    Student_t *result2 = findStudentByNeptunCode(pStudents, numberOfStudents, "QSTSG9");
+    if (result2 == NULL ) {
+        printf("QSTSG9 student doesn't exist");
+    }
+    else {
+        printf("QSTSG9 student: ");
+        printf("\n");
+        printStudent(*result2);
+    }
+    printf("\n");
+    printf("\n");
+
+    sortStudentsByName(pStudents, numberOfStudents);
+    printAllStudents(pStudents, numberOfStudents, "abc_order.txt");
+    printf("\n");
+    printf("\n");
+
+    sortStudentsByAgeAndExam(pStudents, numberOfStudents);
+    printAllStudents(pStudents, numberOfStudents, "birth_exam_order.txt");
+
+    
     return 0;
 }
+
