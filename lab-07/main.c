e15d6eb8c251e9b03ccd59dfcc84440d6fad1da7
kovacsnora <kovacs.nora@student.ms.sapientia.ro>
2025-05-14 13:35:04 +0300

    lab7 labda tomb feladat megoldva

--- lab-07/main.c
+++ lab-07/main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 
+#include "labda_tomb.h"
+
 int main() {
-    printf("Hello, World!\n");
+    LabdaArray array;
+    createArray(20, &array);
+
+    //EGY LABDA ADATAINAK KIIRASA
+    /*
+    Labda labda1;
+    readLabda(&labda1);
+    printLabda(labda1);
+    */
+    
+    readOsszesLabda(&array);
+
+    printOsszesLabda(&array);
+
+    //Vegyük le a polcról a 15 cm átmérőnél nagyobb labdákat, majd ismét írjuk
+    //ki a polc tartalmát.
+    labda_torol(&array);
+
+    printf("\nszures utan: \n");
+
+    printOsszesLabda(&array);
+
+    printf("\nalairt labdak szama: %d\n", szamol_alairt(array));
+
+    printf("\nrendezes utan:\n");
+
+    qsort(array.elements, 20, sizeof(Labda), compare);
+
+    printOsszesLabda(&array);
+
+    deallocateArray(&array);
     return 0;
 }
+
